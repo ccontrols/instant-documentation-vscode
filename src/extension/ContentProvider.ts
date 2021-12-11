094834658e3a3b22709ba6bae1bd1a6fdfd6fba6
atanasster <atanasster@gmail.com>
2021-12-11 16:56:49 +0200

    path-browserify require

--- src/extension/ContentProvider.ts
+++ src/extension/ContentProvider.ts
@@ -1,4 +1,4 @@
-import * as path from 'path';
+import * as path from 'path-browserify';
 import * as vscode from 'vscode';
 import { extractProps } from './extract-props';
 import { getNonce, openLocation } from './utils';
